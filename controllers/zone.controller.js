6262a2b2736c5629b15f2a4fa52a48c3c03d4f19
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-01-16 17:52:31 +0530

    added basic functionality

--- controllers/zone.controller.js
+++ controllers/zone.controller.js
@@ -13,14 +13,20 @@ exports.getZoneInfo = async (req, res) => {
   try {
     const query = `
       SELECT 
-        z.zone_name
+        z.zone_name,
+        ST_Distance(
+          ST_Transform(ST_SetSRID(ST_Point(z.longitude, z.latitude), 4326), 32643),
+          ST_Transform(ST_SetSRID(ST_Point(:longitude, :latitude), 4326), 32643)
+        ) AS distance
       FROM zone_coordinates z
       JOIN map m ON z.zone_id = m.gid
       WHERE ST_Intersects(
         m.geom, 
         ST_Transform(ST_SetSRID(ST_Point(:longitude, :latitude), 4326), 32643)
-      );
-    `;;
+      )
+      ORDER BY distance ASC
+      LIMIT 1;
+    `;
 
     const result = await sequelize.query(query, {
       replacements: { latitude, longitude },
@@ -33,7 +39,7 @@ exports.getZoneInfo = async (req, res) => {
         .json({ message: "No zone found for the given coordinates." });
     }
 
-    res.json(result[0]); // Return only the first matching zone
+    res.json(result[0]); // Return the first matching zone with distance
   } catch (error) {
     console.error("Error fetching zone info:", error);
     res.status(500).json({ error: "Internal Server Error" });
