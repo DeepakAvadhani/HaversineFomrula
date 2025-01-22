7fb6e3d149390772f076aa45c3fb6b06d40411f4
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-01-22 18:43:22 +0530

    updated updateLocation code

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const agentController = require('../controllers/agentController');
-
+const productController = require('../controllers/productController');
 const router = express.Router();
 
 router.post('/update-location', agentController.updateLocation);
 router.get('/sorted-agents', agentController.getSortedAgents);
-
+router.get('/getProduct/:barcode',productController.getProductByBarcode)
 module.exports = router;
