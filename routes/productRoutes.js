c56b454c32e7672ecf5631b4863d0c7df50627c2
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-01-28 12:36:25 +0530

    added product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const agentController = require('../controllers/agentController');
+const productController = require('../controllers/productController');
 const router = express.Router();
 
-router.post('/update-location', agentController.updateLocation);
-router.get('/sorted-agents', agentController.getSortedAgents);
+router.get('/getProduct/:barcode',productController.getProductByBarcode)
 module.exports = router;
